312646d8f36a661b80d6a3be00830589941ad691
Ryan Deng <ryan.deng32@gmail.com>
2019-05-05 19:06:55 -0400

    tsx

---
+++ 32 topdown.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="32 topdown" tilewidth="32" tileheight="32" spacing="5" tilecount="540" columns="27">
+ <image source="../Projects/current_pygame_project/img/spritesheet_tiles copy.png" width="994" height="735"/>
+</tileset>
