17336c86eee9042f301eaabae102a1412edc21ff
Ryan Deng <ryan.deng32@gmail.com>
2020-08-26 15:43:06 -0400

    clean up file structure

--- map/32 topdown.tsx
+++ map/32 topdown.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.3" name="32 topdown" tilewidth="32" tileheight="32" spacing="5" tilecount="540" columns="27">
- <image source="./img/spritesheet_tiles copy.png" width="994" height="735"/>
+ <image source="../img/spritesheet_tiles copy.png" width="994" height="735"/>
 </tileset>
